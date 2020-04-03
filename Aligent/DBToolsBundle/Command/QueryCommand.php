535f77cbe45d03b4b7fcd3050e5c2fcd57c3cb14
Adam Hall <adam.hall@aligent.com.au>
2020-04-03 11:39:27 +1030

    Migrate QueryCommand to use new connection provider Fix Arguments for InfoCommand

--- Aligent/DBToolsBundle/Command/QueryCommand.php
+++ Aligent/DBToolsBundle/Command/QueryCommand.php
@@ -1,12 +1,15 @@
 <?php
 namespace Aligent\DBToolsBundle\Command;
 
+use Aligent\DBToolsBundle\Provider\DatabaseConnectionProvider;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Process\Process;
 
 /**
  * Query Command - Runs a query on the mysql database that Oro is using.
@@ -19,11 +22,30 @@ use Symfony\Component\Console\Question\Question;
  * @link      http://www.aligent.com.au/
  **/
 
-class QueryCommand extends AbstractCommand
+class QueryCommand extends Command
 {
     const COMMAND_NAME = 'oro:db:query';
     const COMMAND_DESCRIPTION=  'Performs a query on the mysql database';
 
+    /**
+     * @var DatabaseConnectionProvider
+     */
+    protected $connectionProvider;
+
+    /**
+     * ConsoleCommand constructor.
+     * @param DatabaseConnectionProvider $connectionProvider
+     */
+    public function __construct(
+        DatabaseConnectionProvider $connectionProvider
+    ) {
+        $this->connectionProvider = $connectionProvider;
+        parent::__construct();
+    }
+
+    /**
+     * Configures the name, arguments and options of the command
+     */
     public function configure() {
         $this->setName(self::COMMAND_NAME)
             ->setDescription(self::COMMAND_DESCRIPTION)
@@ -31,6 +53,11 @@ class QueryCommand extends AbstractCommand
             ->addOption('only-command', null, InputOption::VALUE_NONE, 'Prints only the command. Does not Execute.');
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|void
+     */
     public function execute(InputInterface $input, OutputInterface $output) {
 
         $query = $input->getArgument('query');
@@ -39,14 +66,20 @@ class QueryCommand extends AbstractCommand
         $question = new Question( '<question>SQL:</question>[<comment>' . $query . '</comment>]', $query);
         $query = $helper->ask($input, $output, $question);
 
-        $command = $this->database->getMysqlConnectionString('mysql', [
-            '-e\'' . $query . '\''
-        ]);
+        $connection = $this->connectionProvider->getConnection();
+        $command = $connection->getConnectionString(
+            'mysql',
+            [
+                '-e\'' . $query . '\''
+            ]
+        );
 
         if ($input->getOption('only-command')) {
             $output->writeln($command);
         } else {
-            $this->processCommand($command);
+            $process = new Process($command);
+            $process->mustRun();
+            $output->writeln($process->getOutput());
         }
     }
 }
