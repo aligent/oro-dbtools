86d8685035f4ae7a50372d24c1799aefb3d3f479
Adam Hall <adam.hall@aligent.com.au>
2020-04-09 13:45:44 +0930

    Updated Dump command to use mysqldump-php library

--- src/Command/DumpCommand.php
+++ src/Command/DumpCommand.php
@@ -2,10 +2,10 @@
 namespace Aligent\DBToolsBundle\Command;
 
 use Aligent\DBToolsBundle\Database\DatabaseConnectionInterface;
-use Aligent\DBToolsBundle\Compressor\CompressorInterface;
 use Aligent\DBToolsBundle\Helper\VerifyOrDie;
-use Aligent\DBToolsBundle\Provider\CompressionServiceProvider;
 use Aligent\DBToolsBundle\Provider\DatabaseConnectionProvider;
+use Aligent\DBToolsBundle\Sanitizer\Sanitizer;
+use Ifsnop\Mysqldump\Mysqldump;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputArgument;
@@ -13,7 +13,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
-use Symfony\Component\Process\Process;
+use Symfony\Component\VarDumper\VarDumper;
 
 /**
  * Dump Command - Dumps the database to a file
@@ -42,34 +42,21 @@ class DumpCommand extends Command
     protected $connection;
 
     /**
-     * @var CompressorInterface
-     */
-    protected $compressor;
-
-    /**
      * @var DatabaseConnectionProvider
      */
     protected $connectionProvider;
 
     /**
-     * @var CompressionServiceProvider
-     */
-    protected $compressionProvider;
-
-    /**
      * ConsoleCommand constructor.
      * @param DatabaseConnectionProvider $connectionProvider
-     * @param CompressionServiceProvider $compressionProvider
      * @param array $definitions
      */
     public function __construct(
         DatabaseConnectionProvider $connectionProvider,
-        CompressionServiceProvider $compressionProvider,
         array $definitions
     ) {
         $this->connectionProvider = $connectionProvider;
-        $this->compressionProvider = $compressionProvider;
-        $this->tableDefinitions = $definitions;
+        $this->definitions = $definitions;
         parent::__construct();
     }
 
@@ -82,6 +69,12 @@ class DumpCommand extends Command
             ->setDescription(self::COMMAND_DESCRIPTION)
             ->addArgument('filename', InputArgument::OPTIONAL, 'Dump filename')
             ->addOption(
+                'sanitize',
+                's',
+                InputOption::VALUE_REQUIRED,
+                'Which sanitization definition to use.'
+            )
+            ->addOption(
                 'add-time',
                 't',
                 InputOption::VALUE_OPTIONAL,
@@ -93,12 +86,8 @@ class DumpCommand extends Command
                 InputOption::VALUE_REQUIRED,
                 'Compress the dump file using one of the supported algorithms'
             )
-            ->addOption(
-                'only-command',
-                null,
-                InputOption::VALUE_NONE,
-                'Print only mysqldump command. Do not execute'
-            );
+            ->addOption('exclude', null, InputOption::VALUE_OPTIONAL, 'Comma separated list of tables to exclude from the dump')
+            ->addOption('include', null, InputOption::VALUE_OPTIONAL, 'Comma separated list of tables to include in the dump');;
     }
 
     /**
@@ -109,12 +98,6 @@ class DumpCommand extends Command
     {
         parent::initialize($input, $output);
 
-        if ($input->getOption('compression')) {
-            $this->compressor = $this->compressionProvider->getCompressor($input->getOption('compression'));
-        } else {
-            $this->compressor = $this->compressionProvider->getCompressor('none');
-        }
-
         $this->connection = $this->connectionProvider->getConnection();
     }
 
@@ -122,39 +105,90 @@ class DumpCommand extends Command
      * @param InputInterface $input
      * @param OutputInterface $output
      * @return int|void
+     * @throws \Exception
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $fileName = $this->getFileName($input, $output);
-        $canOutputInformation = !$input->getOption('only-command');
+        $dumpSettings = [];
 
+        $compression = $input->getOption('compression');
+        if ($compression) {
+            if (strtolower($compression) !== strtolower(Mysqldump::GZIP)) {
+                throw new \InvalidArgumentException("$compression is not a supported.");
+            }
 
-        if ($canOutputInformation) {
-            $this->writeSection($output, 'Dumping Database');
+            $dumpSettings['compress'] = Mysqldump::GZIP;
         }
 
-        $dumpCommand = $compressor->getCompressingCommand($this->connection->getDumpDatabaseCommand());
-        $dumpCommand .= ' > ' . escapeshellarg($fileName);
+        // If we're sanitizing get which definition to use
+        $sanitizer = $input->getOption('sanitize');
+        if ($sanitizer) {
+            if (!isset($this->definitions[$sanitizer])) {
+                throw new \InvalidArgumentException($sanitizer . ' does not exist. Check for valid values using the list-definitions command.');
+            }
 
-        if ($input->getOption('only-command')) {
-            $output->writeln($dumpCommand);
-        } else {
-            $process = new Process(
-                $dumpCommand,
-                null,
-                null,
-                null,
-                null
-            );
+            $definition = $this->definitions[$sanitizer];
+            $dumpSettings['no-data'] = $definition['truncate'] ?? [];
+        }
 
-            try {
-                $process->mustRun();
-                $output->writeln($process->getOutput());
-            } catch (\Exception $e) {
-                $output->writeln($process->getErrorOutput());
-                $output->writeln($e->getMessage());
+        if ($input->getOption('exclude')) {
+            $dumpSettings['exclude-tables'] = explode(',', $input->getOption('exclude'));
+            if ($output->isVerbose()) {
+                $output->writeln(
+                    sprintf('<comment>Excluded: <info>%s</info></comment>', implode(', ', $dumpSettings['exclude-tables']))
+                );
             }
         }
+
+        if ($input->getOption('include')) {
+            $dumpSettings['include-tables'] = explode(',', $input->getOption('include'));
+
+            if ($output->isVerbose()) {
+                $output->writeln(
+                    sprintf('<comment>Included: <info>%s</info></comment>', implode(', ', $dumpSettings['include-tables']))
+                );
+            }
+        }
+
+        // Fetch the configured dumper from our connection
+        $dumper = $this->connection->getDumper($dumpSettings);
+
+        if ($sanitizer) {
+            $updates = $definition['update'] ?? [];
+            $sanitizer = new Sanitizer();
+
+            $dumper->setTransformTableRowHook(
+                function ($tableName, array $row) use ($sanitizer, $updates) {
+                    if (isset($updates[$tableName])) {
+                        $columns = $updates[$tableName]['columns'] ?? [];
+                        foreach ($columns as $column => $function) {
+                            if (!array_key_exists($column, $row)) {
+                                throw new \InvalidArgumentException("$column does not exist on $tableName, cannot sanitize a column that does not exist.");
+                            }
+
+                            if (isset($row[$column])) {
+                                $row[$column] = $sanitizer->sanitize($function, $row[$column]);
+                            }
+                        }
+                    }
+
+                    return $row;
+                }
+            );
+        }
+
+        $dumper->setInfoHook(
+            function ($object, $info) use ($output) {
+                if ($output->isVeryVerbose()) {
+                    if ($object === 'table') {
+                        $output->writeln("Dumped Table " . $info['name'] . ", Rows: " . $info['rowCount']);
+                    }
+                }
+            }
+        );
+
+        $fileName = $this->getFileName($input, $output);
+        $dumper->start($fileName);
     }
     
 
@@ -181,21 +215,28 @@ class DumpCommand extends Command
         }
 
         if (($fileName = $input->getArgument('filename')) === null || ($isDir = is_dir($fileName))) {
-
             $defaultName = VerifyOrDie::filename(
                 $namePrefix . $this->connection->getName() . $nameSuffix . $nameExtension
             );
+
             if (isset($isDir) && $isDir) {
                 $defaultName = rtrim($fileName, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . $defaultName;
             }
-            if (!$input->getOption('force')) {
-                /** @var QuestionHelper $helper */
-                $helper = $this->getHelper('question');
-                $question = new Question( '<question>Filename for SQL dump:</question> [<comment>' . $defaultName . '</comment>]', $defaultName);
-                $fileName = $helper->ask($input, $output, $question);
-            } else {
-                $fileName = $defaultName;
+
+            if($input->getOption('compression')) {
+                if (substr($defaultName, -3, 3) === '.gz') {
+                    return $defaultName;
+                } elseif (substr($defaultName, -4, 4) === '.sql') {
+                    $defaultName .= '.gz';
+                } else {
+                    $defaultName .= '.sql.gz';
+                }
             }
+
+            /** @var QuestionHelper $helper */
+            $helper = $this->getHelper('question');
+            $question = new Question( '<question>Filename for SQL dump:</question> [<comment>' . $defaultName . '</comment>]', $defaultName);
+            $fileName = $helper->ask($input, $output, $question);
         } else {
             if ($input->getOption('add-time')) {
                 $pathParts = pathinfo($fileName);
@@ -203,8 +244,6 @@ class DumpCommand extends Command
                     $namePrefix . $pathParts['filename'] . $nameSuffix . '.' . $pathParts['extension'];
             }
         }
-
-        $fileName = $this->compressor->getFileName($fileName);
 
         return $fileName;
     }
